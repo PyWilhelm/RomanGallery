e25b6da601988a954785fa106452f51858c2ffb1
liziyang <liziyang@zhihu.com>
2017-01-15 21:29:57 +0800

    Validate token

--- gallery/views/views.py
+++ gallery/views/views.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
+import hashlib
+
 from flask import request
 
 from gallery.main import app
 
 
-@app.route('/')
+@app.route('/wx')
 def main_handler():
-    data = request.data
-    print data
-    return ''
+    signature = request.args.get('signature')
+    timestamp = request.args.get('signature')
+    nonce = request.args.get('nonce')
+    echostr = request.args.get('echostr')
+    token = 'MemoryGallery'
+    ls = [token, timestamp, nonce]
+    ls.sort()
+    sha1 = hashlib.sha1()
+    map(sha1.update, ls)
+    hashcode = sha1.hexdigest()
+    print "handle/GET func: hashcode, signature: ", hashcode, signature
+    if hashcode == signature:
+        return echostr
+    else:
+        return ""
